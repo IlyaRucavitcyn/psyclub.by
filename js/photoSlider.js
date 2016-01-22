a464e57803cfc7947eaae79689d30e80b2a08cc9
Ilya Rucavitcyn <ilya.rucavitcyn@gmail.com>
2016-01-22 16:35:17 +0300

    Promo photoes setting #3

--- js/photoSlider.js
+++ js/photoSlider.js
@@ -5,12 +5,23 @@ function setElementHeight (element, height){
 function setElementWidth (element, width){
   element.width(width);
 }
+function setElementLeft(element, marginLeft){
+  element.css("left", marginLeft);
+}
+function setElementTop(element, marginTop){
+  element.css("top", marginTop);
+}
 
 setElementHeight($('#photo-holder'),$(window).height());
 setElementWidth($('.visual-container .image-frame'),$(window).width());
+setElementHeight($('.visual-container .image-frame'),$(window).height());
+setElementLeft($('.visual-container .image-frame'),-$(window).width()/4);
+setElementTop($('.visual-container .image-frame'),$(window).height()/2-$('.visual-container .image-frame').height()/2);
 
 
 $(window).on("resize", function () {
   setElementHeight($('#photo-holder'),$(window).height());
   setElementWidth($('.visual-container .image-frame'),$(window).width());
+  setElementLeft($('.visual-container .image-frame'),-$(window).width()/4);
+  setElementTop($('.visual-container .image-frame'),$(window).height()/2-$('.visual-container .image-frame').height()/2);
 });
