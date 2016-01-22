842538683d5bc9c9410f1e946ceb0f03d4edf5ee
Ilya Rucavitcyn <ilya.rucavitcyn@gmail.com>
2016-01-22 15:52:27 +0300

    Promo photoes setting #2

--- js/photoSlider.js
+++ js/photoSlider.js
@@ -2,9 +2,15 @@
 function setElementHeight (element, height){
   element.height(height);
 }
+function setElementWidth (element, width){
+  element.width(width);
+}
 
 setElementHeight($('#photo-holder'),$(window).height());
+setElementWidth($('.visual-container .image-frame'),$(window).width());
+
 
 $(window).on("resize", function () {
-  setElementHeight($('#photo-holder'),$(window).height())
+  setElementHeight($('#photo-holder'),$(window).height());
+  setElementWidth($('.visual-container .image-frame'),$(window).width());
 });
