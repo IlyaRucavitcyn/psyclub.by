904dacc576a0edd4099741a986c97f84c4301d62
Ilya Rucavitcyn <ilya.rucavitcyn@gmail.com>
2016-01-23 01:23:56 +0300

    Margin top is set with the function

--- js/photoSlider.js
+++ js/photoSlider.js
@@ -1,39 +1,41 @@
 // var $photoContainer = $('#photo-holder');
-function setElementHeight (element, height){
+function setElementHeight (element, height) {
   element.height(height);
 }
-function setElementWidth (element, width){
+
+function setElementWidth (element, width) {
   element.width(width);
 }
-function setElementLeft(element, marginLeft){
+
+function setElementLeft(element, marginLeft) {
   element.css("left", marginLeft);
 }
 
-
-  if ($('.visual-container .image-frame img').height() >= $('.visual-container .image-frame').height()) {
-    $('.visual-container .image-frame img').css({
+function setImageTop (image, holder) {
+  if (image.height() >= holder.height()) {
+    image.css({
       width: '100%',
       height: 'auto',
       marginLeft:'',
-      marginTop: ($('.visual-container .image-frame').height() - $('.visual-container .image-frame img').height())/2
+      marginTop: (holder.height() - image.height())/2
     })
   } else {
-    $('.visual-container .image-frame img').css({
+    image.css({
       width: 'auto',
       height: '100%',
       marginTop:''
       });
-    $('.visual-container .image-frame img').css({
-      marginLeft: ($('.visual-container .image-frame').width() - $('.visual-container .image-frame img').width())/2
+    image.css({
+      marginLeft: (holder.width() - image.width())/2
       });
   }
-
+}
 
 setElementHeight($('#photo-holder'),$(window).height());
 setElementWidth($('.visual-container .image-frame'),$(window).width());
 setElementHeight($('.visual-container .image-frame'),$(window).height());
 setElementLeft($('.visual-container .image-frame'),-$(window).width()/4);
-// setElementTop($('.visual-container .image-frame'),$(window).height()/2-$('.visual-container .image-frame').height()/2);
+setImageTop($('.visual-container .image-frame img'), $('.visual-container .image-frame'));
 
 
 $(window).on("resize", function () {
@@ -41,23 +43,5 @@ $(window).on("resize", function () {
   setElementWidth($('.visual-container .image-frame'),$(window).width());
   setElementHeight($('.visual-container .image-frame'),$(window).height());
   setElementLeft($('.visual-container .image-frame'),-$(window).width()/4);
-  // setElementTop($('.visual-container .image-frame'),$(window).height()/2-$('.visual-container .image-frame').height()/2);
-
-  if ($('.visual-container .image-frame img').height() >= $('.visual-container .image-frame').height()) {
-    $('.visual-container .image-frame img').css({
-      width: '100%',
-      height: 'auto',
-      marginLeft:'',
-      marginTop: ($('.visual-container .image-frame').height() - $('.visual-container .image-frame img').height())/2
-    })
-  } else {
-    $('.visual-container .image-frame img').css({
-      width: 'auto',
-      height: '100%',
-      marginTop:''
-      });
-    $('.visual-container .image-frame img').css({
-      marginLeft: ($('.visual-container .image-frame').width() - $('.visual-container .image-frame img').width())/2
-      });
-  }
+  setImageTop($('.visual-container .image-frame img'), $('.visual-container .image-frame'));
 });
