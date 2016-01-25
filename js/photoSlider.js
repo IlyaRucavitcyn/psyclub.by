c8291eed679def818a1ee5d573b506f2e5428407
Ilya Rucavitcyn <ilya.rucavitcyn@gmail.com>
2016-01-25 12:11:33 +0300

    Photo slider #1

--- js/photoSlider.js
+++ js/photoSlider.js
@@ -11,6 +11,10 @@ function setElementLeft(element, marginLeft) {
   element.css("left", marginLeft);
 }
 
+function setElementRight(element, marginRight) {
+  element.css("right", marginRight);
+}
+
 function setImageTop (image, holder) {
   if (image.height() >= holder.height()) {
     image.css({
@@ -31,6 +35,11 @@ function setImageTop (image, holder) {
   }
 }
 
+function slideImage (a,b) {
+  a.css("width",'100%');
+  b.css("width","0px");
+}
+
 setElementHeight($('#photo-holder'),$(window).height());
 setElementWidth($('.visual-container .image-frame'),$(window).width());
 setElementHeight($('.visual-container .image-frame'),$(window).height());
@@ -45,3 +54,13 @@ $(window).on("resize", function () {
   setElementLeft($('.visual-container .image-frame'),-$(window).width()/4);
   setImageTop($('.visual-container .image-frame img'), $('.visual-container .image-frame'));
 });
+
+$($(".visual-container .left-section")).on("click", function () {
+  slideImage($(".visual-container .left-section"), $(".visual-container .right-section"));
+  setElementLeft($('.visual-container .image-frame'), '0px');
+})
+
+$($(".visual-container .right-section")).on("click", function () {
+  setElementLeft($('.visual-container .image-frame'), '0px');
+  slideImage($(".visual-container .right-section"), $(".visual-container .left-section"));
+})
