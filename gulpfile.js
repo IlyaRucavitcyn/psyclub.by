1d5ae6babf4b6b76637fee195d3a588ccc2bfb7a
Ilya Rucavitcyn <ilya.rucavitcyn@gmail.com>
2016-01-28 15:35:10 +0300

    Gulp plugings are installed

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+// var gulp = require('gulp');
+// var csslint = require('gulp-csslint');
+// var jslint = require('gulp-jslint');
+// var concatCss = require('gulp-concat-css');
+// var concat = require('gulp-concat');
+// var cssmin = require('gulp-cssmin');
+// var uglify = require('gulp-uglify');
+// var rename = require("gulp-rename");
+// var ghPages = require('gulp-gh-pages');
+// var debug = require('gulp-debug');
+//
+//
+// gulp.task('csslint', function() {
+//   gulp.src('src/**/*.css')
+//     .pipe(csslint({
+//       'adjoining-classes':false
+//     }))
+//     .pipe(csslint.reporter());
+// });
+//
+// gulp.task('jslint', function () {
+//   return gulp.src(['src/js_files/*.js'])
+//     .pipe(jslint({
+//         node: true,
+//         evil: true,
+//         nomen: true,
+//         global: [],
+//         predef: [],
+//         reporter: 'default',
+//         edition: '2014-07-08',
+//         errorsOnly: false
+//     }))
+//     .on('error', function (error) {
+//             console.error(String(error));
+//     });
+// });
+//
+// gulp.task('concatcss', function () {
+//   return gulp.src('src/css_files/*.css')
+//     .pipe(concatCss('bundle.css'))
+//     .pipe(gulp.dest('css/'));
+// });
+//
+// gulp.task('concatjs', function() {
+//   return gulp.src('src/js_files/*.js')
+//     .pipe(concat('all.js'))
+//     .pipe(gulp.dest('js/'));
+// });
+//
+// gulp.task('cssmin', function () {
+// 	gulp.src('src/css_files/*.css')
+// 		.pipe(cssmin())
+// 		.pipe(gulp.dest('css/*.css'));
+// });
+//
+// gulp.task('jsmin', function() {
+//   return gulp.src('js/*.js')
+//     .pipe(uglify())
+//     .pipe(gulp.dest('src/js_files/*.js'));
+// });
+//
+// gulp.task('watch', function() {
+//   gulp.watch(['src/**/*.css','src/**/*.js'], ['jslint', 'concatjs', 'csslint', 'concatcss']);
+// });
+//
+// // gulp.task('watch', function() {
+// //   gulp.watch('src/css_files/*.css', ['csslint', 'concatcss']);
+// // });
+//
+// gulp.task('deploy',['cssmin','jsmin'], function() {
+//   return gulp.src('./**/*.*')
+//     .pipe(ghPages())
+//     // .pipe(debug({title: 'debug:'}));
+// });
+//
+// gulp.task('default',['csslint', 'concatcss', 'jslint', 'concatjs', 'watch']);
